30c47090a7bbd289399af9b8ba75e84e8ad0691b
Jacob Reinhold <jacob.reinhold@jhu.edu>
2019-06-17 15:39:59 -0400

    revert back to sequential for final in hotnet, output all uncert in one run

--- annom/models.py
+++ annom/models.py
@@ -120,42 +120,35 @@ class HotNet(Unet):
     """
     defines a 2d or 3d uncertainty-calculating unet based on vanilla regression in pytorch
     """
-    def __init__(self, n_layers:int, monte_carlo:int=50, min_logvar:float=np.log(1e-6), laplacian:bool=True,
-                 uncertainty:str='predictive', **kwargs):
+    def __init__(self, n_layers:int, monte_carlo:int=50, min_logvar:float=np.log(1e-6), laplacian:bool=True, **kwargs):
         self.n_samp = monte_carlo or 50
         self.mlv = min_logvar
         self.laplacian = laplacian
-        self.uncertainty = uncertainty if uncertainty is not None else 'predictive'
-        if self.uncertainty not in ('predictive', 'aleatoric', 'epistemic'):
-            raise AnnomError(f'Uncertainty type: {self.uncertainty}, not valid')
-        logger.debug(f'Uncertainty type: {self.uncertainty}')
         super().__init__(n_layers, enable_dropout=True, **kwargs)
         self.criterion = HotLoss() if not laplacian else HotLaplacianLoss()
 
     def _finish(self, x:torch.Tensor) -> Tuple[torch.Tensor,torch.Tensor]:
-        xh = self.finish[0][1](self._add_noise(self.finish[0][0](x)))
-        s = torch.clamp(self.finish[1][1](self._add_noise(self.finish[1][0](x))), min=self.mlv)
+        xh = self.finish[0](x)
+        s = torch.clamp(self.finish[1](x), min=self.mlv)
         return xh, s
 
     def _final(self, in_c:int, out_c:int, out_act:Optional[str]=None, bias:bool=False):
         ksz = (3,3,3) if self.init_3d else self.kernel_sz
         kszf = tuple([1 for _ in self.kernel_sz])
-        f = nn.ModuleList([self._conv_act(in_c, in_c, ksz, self.act, self.norm),
-                           self._conv(in_c, out_c, kszf, bias=bias)])
-        s = nn.ModuleList([self._conv_act(in_c, in_c, ksz, self.act, self.norm),
-                           self._conv(in_c, out_c, kszf, bias=bias)])
+        f = nn.Sequential(self._conv_act(in_c, in_c, ksz, self.act, self.norm),
+                          self._conv(in_c, out_c, kszf, bias=bias))
+        s = nn.Sequential(self._conv_act(in_c, in_c, ksz, self.act, self.norm),
+                          self._conv(in_c, out_c, kszf, bias=bias))
         return nn.ModuleList([f, s])
 
-    def _calc_uncertainty(self, yhat, s) -> torch.Tensor:
+    def _calc_uncertainty(self, yhat, s) -> Tuple[torch.Tensor,torch.Tensor]:
         epistemic = torch.mean(yhat**2,dim=0) - torch.mean(yhat,dim=0)**2
         aleatoric = torch.mean(torch.exp(s),dim=0) if not self.laplacian else torch.mean(2*torch.exp(s)**2,dim=0)
-        if self.uncertainty == 'epistemic': return epistemic
-        elif self.uncertainty == 'aleatoric': return aleatoric
-        else: return (epistemic + aleatoric)
+        return epistemic, aleatoric
 
     def predict(self, x:torch.Tensor, return_temp:bool=False, **kwargs) -> torch.Tensor:
         out = [self.forward(x) for _ in range(self.n_samp)]
-        yhat = torch.stack([o[0] for o in out])
-        s = torch.stack([o[1] for o in out])
-        if return_temp: return self._calc_uncertainty(yhat, s)
-        return torch.mean(yhat, dim=0)
+        yhat = torch.stack([o[0] for o in out]).cpu().detach()
+        s = torch.stack([o[1] for o in out]).cpu().detach()
+        e, a = self._calc_uncertainty(yhat, s)
+        return torch.cat((torch.mean(yhat, dim=0), e, a), dim=1)
